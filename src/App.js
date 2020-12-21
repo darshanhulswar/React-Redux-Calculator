2a8e673bf261c3a8251ae65b5ed5b9afa3d1fbe6
Darshan <dhulswar591@gmail.com>
2020-12-21 22:14:14 +0530

    useSelector added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 
-export class App extends Component {
-  render() {
-    let result = 0;
+const App = () => {
+  let result = 0;
+  const finalResult = useSelector((state) => state.result);
+  console.log(finalResult);
 
-    return (
-      <div className="Calculator">
-        <h2>Calculator</h2>
-        <input type="text" readOnly value={result} />
-        <div className="keypad">
-          <div className="Numeric__input">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-          </div>
-          <div className="Math__input">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>/</button>
-            <button className="special_class">=</button>
-          </div>
+  return (
+    <div className="Calculator">
+      <h2>Calculator</h2>
+      <input type="text" readOnly value={result} />
+      <div className="keypad">
+        <div className="Numeric__input">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+        </div>
+        <div className="Math__input">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button className="special_class">=</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,12 @@ import reducer from "../reducers";
 
 const initialState = {
   result: 0,
+  tempVariable: 0,
+  operation: "",
 };
 
-export const store = createStore(reducer, initialState);
+export const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
